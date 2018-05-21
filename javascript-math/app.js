2800154c428393c1fe894524f79e00f827b1b3a6
Ilgu Lee <iglee2000ca@gmail.com>
2018-05-20 21:27:38 -0400

    Init

---
+++ javascript-math/app.js
@@ -0,0 +1,20 @@
+// val = Math.PI;
+// val = Math.E;
+// val = Math.round(2.4);
+// val = Math.ceil(2.4);
+// val = Math.floor(2.8);
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8, 2);
+// val = Math.min(2,33,4,1,55,6,3,-2);
+// val = Math.max(2,33,4,1,55,6,3,-2);
+
+var val = Math.random();//0.00000~0.999999
+var ten=val * 10//0.0000~9.99999
+var oneten=ten + 1; //1.0000~10.99999
+var rand = Math.floor(oneten);
+
+console.log(val);
+console.log(ten);
+console.log(oneten);
+console.log(rand);
